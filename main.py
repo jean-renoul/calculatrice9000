14c4734c48f4fff0996cba1489c0f9884a97e6c4
jean-renoul <jean.renoul@laplateforme.io>
2023-12-01 09:23:29 +0100

    ajout des commentaires

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
+# Liste pour stocker les nombres et l'historique des opérations
 nombres = []
 historique = []
 
+# Fonction pour saisir les nombres et les opérateurs
 def data():
-
+    # Saisir le premier nombre
     a = input("Entrez un nombre : ")
     nombres.append(a)
     historique.append(a)
+
+    # Saisir les nombres ou opérateurs jusqu'à ce que "=" soit entré
     while True:
         a = input("Entrez un nombre ou un opérateur : ")
         nombres.append(a)
@@ -13,26 +17,30 @@ def data():
         if a == "=":
             return nombres
 
+# Fonction principale pour effectuer les calculs
 def calculette():
-
+    # Appeler la fonction pour saisir les données
     data()
+
+    # Première boucle pour traiter les multiplications et divisions
     i = 1
     while i < len(nombres):
         if nombres[i] == "*":
             nombres[i - 1] = (float(nombres[i - 1]) * float(nombres[i + 1]))
             del nombres[i:i+2]
         elif nombres[i] == "/":
+            # Interdire la division par zéro
             if nombres[i + 1] == "0":
-                print ("Vous ne pouvez pas diviser par 0")
+                print("Vous ne pouvez pas diviser par 0")
                 nombres.clear()
                 calculette()
-
             else:
                 nombres[i - 1] = (float(nombres[i - 1]) / float(nombres[i + 1]))
                 del nombres[i:i+2]
         else:
             i += 1
 
+    # Deuxième boucle pour traiter les additions et soustractions
     i = 1
     while i < len(nombres):
         if nombres[i] == "+":
@@ -43,26 +51,30 @@ def calculette():
             del nombres[i:i+2]
         else:
             i += 1
-    print (float(nombres[0]))
+
+    # Afficher le résultat, ajouter le résultat à l'historique et nettoyer la liste nombres
+    print(float(nombres[0]))
     historique.append(float(nombres[0]))
     nombres.clear()
 
-
+    # Demander à l'utilisateur s'il veut afficher l'historique
     afficher_historique = input("Voulez-vous afficher l'historique ? (oui/non)")
     if afficher_historique == "oui":
-        print (historique)
+        print(historique)
     else:
         calculette()
 
-    input_effacer_historique = input ("Voulez-vous supprimer cet historique ? (oui/non)")
+    # Demander à l'utilisateur s'il veut effacer l'historique
+    input_effacer_historique = input("Voulez-vous supprimer cet historique ? (oui/non)")
     if input_effacer_historique == "oui":
         effacer_historique()
         calculette()
     else:
         calculette()
 
-
+# Fonction pour effacer l'historique
 def effacer_historique():
     historique.clear()
 
+# Appeler la fonction principale pour démarrer la calculatrice
 calculette()
